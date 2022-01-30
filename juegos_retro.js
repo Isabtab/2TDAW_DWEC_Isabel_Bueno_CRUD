5c3f41117427c8cdaba2e82ad67e64ab96e8f3da
Isabtab <91192800+Isabtab@users.noreply.github.com>
2022-01-30 16:01:16 +0100

    Add files via upload

---
+++ juegos_retro.js
@@ -0,0 +1,81 @@
+let ultimo_id=6;
+let juegos_retro=[
+   {
+   	"id":1,
+      "titulo":"Final Fantasy VI",
+   	"genero":"JRPG",
+      "descripcion":"Uno de los mejores juegos de rol jamás creado",
+      "plataformas":"Super Nintendo Entertainment System,PlayStation 1,GameBoy Advance",
+      "guia":"https://www.ffmaniacs.com/ff6/",
+      "portada":"https://www.eliteguias.com/img/guias/final-fantasy-vi.jpg",
+      "lanzamiento":"1994-04-02",
+      "valoracion": 10.00
+   },
+
+    {
+      "id":2,
+      "titulo":"The Secret of Monkey Island",
+      "genero":"Aventura gráfica",
+      "descripcion":"Aventura gráfica de las que ya no se hacen hoy en día",
+      "plataformas":"Windows, plataformas soportadas por ScummVM",
+      "guia":"https://www.eliteguias.com/guias/t/tsomi/the-secret-of-monkey-island-p2.php",
+      "portada":"https://i.imgur.com/BphXrHv.jpg",
+      "lanzamiento":"1997-10-31",
+      "valoracion": 8.50
+   },
+
+ {
+      "id":3,
+      "titulo":"The Revenge of Shinobi",
+      "genero":"Acción, plataformas",
+      "descripcion":"Segundo título de la saga Shinobi",
+      "plataformas":"Mega Drive, Consola Virtual,Xbox Live Arcade,PlayStation Network,Android,iOS",
+      "guia":"http://www.retrojugones.com/p/the-revenge-of-shinobi-guia.html",
+      "portada":"https://img.zonared.com/images/analisis/700/707/1.jpg",
+      "lanzamiento":"1989-12-02",
+      "valoracion": 8.00
+   },
+
+   {
+      "id":4,
+      "titulo":"Chrono Trigger",
+      "genero":"JRPG",
+      "descripcion":"Uno de los mejores juegos del género JRPG",
+      "plataformas":"Super Nintendo Entertainment System, PlayStation 1, Nintendo DS, Steam, Android, iOS",
+      "guia":"https://gamefaqs.gamespot.com/snes/563538-chrono-trigger/faqs/44737",
+      "portada":"https://as.com/meristation/imagenes/2018/04/04/noticia/1522815300_971031_1531816576_sumario_normal.jpg",
+      "lanzamiento":"1995-03-11",
+      "valoracion": 10.00
+   },
+
+
+{
+      "id":5,
+      "titulo":"Sonic the Hedgehog",
+      "genero":"Aventura, plataformas",
+      "descripcion":"Primer juego de Sonic the Hedgehog",
+      "plataformas":"Mega Drive, Master System",
+      "guia":"http://www.retrojugones.com/p/sonic-hedgehog.html",
+      "portada":"https://www.retroplace.com/pics/genesis/packshots/177433--sonic-the-hedgehog.png",
+      "lanzamiento":"1991-06-23",
+      "valoracion": 7.00
+   },
+
+{
+      "id":6,
+      "titulo":"The Legend of Zelda: A Link to the Past",
+      "genero":"Aventura",
+      "descripcion":"Juego de culto de la saga The Legend of Zelda",
+      "plataformas":"Super Nintendo Entertainment System, Game Boy Advance",
+      "guia":"https://www.guiasnintendo.com/1_GAMEBOY_ADVANCE/Zelda_GBA/Zelda_GBA_Guia/welcome.html",
+      "portada":"https://upload.wikimedia.org/wikipedia/en/2/21/The_Legend_of_Zelda_A_Link_to_the_Past_SNES_Game_Cover.jpg",
+      "lanzamiento":"1991-11-21",
+      "valoracion": 9.80
+   },
+
+  
+
+
+   
+]
+
